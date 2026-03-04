7b143202671a407c30f46062fed03a4a081c3ed7
andrééé <andreestevesarantes19@gmail.com>
2026-03-04 20:33:34 -0300

    feat: add functions to red categoric and numeric variables

--- main.py
+++ main.py
@@ -4,7 +4,27 @@ from ucimlrepo import fetch_ucirepo
 # Wine quality
 dataset = fetch_ucirepo(id=186)
 
-X = dataset.data.features
-y = dataset.data.targets
+CATEGORIC = dataset.data.targets
+NUMERIC = dataset.data.features
 
-print(X.head())
+def get_categoric(target: any):
+    freq_abs = target.value_counts()
+    freq_rel = target.value_counts(normalize=True) * 100
+
+    return pd.DataFrame({
+        'Frequência Absoluta': freq_abs,
+        'Frequência Relativa (%)': freq_rel
+    })
+
+def get_numeric(feature: any) -> str:
+    coluna = feature['sepallength']
+    classes = 5
+
+    tabela_num = pd.cut(coluna, bins=classes).value_counts().sort_index()
+
+def main():
+    get_categoric(CATEGORIC)
+    get_numeric(NUMERIC)
+
+if __name__ == '__main__':
+    main()
